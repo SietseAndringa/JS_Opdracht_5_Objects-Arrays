0477080e0744b230460465a7ce7753845724ab6a
SietseAndringa <82322340+SietseAndringa@users.noreply.github.com>
2021-10-19 15:12:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'Sietse',
+    age: 36,
+    evaluations: [7, 10, 9]  
+};
+
+let colors = ['red', 'green', 'blue'];
+let hello = {greeting:'Hi ik ben een object'};
+colors.push('orange', 5, hello);
+let last = colors[colors.length - 1];
+
+console.log(last);
